25dabf1eadf82c1455a1428de9c926c6f35575a2
Renz Gabriel E. Salas <124060701+RenzGabrielSalas@users.noreply.github.com>
2025-03-28 06:46:05 +0800

    Create options.dart

---
+++ lib/config/options.dart
@@ -0,0 +1,16 @@
+class OptionSettings {
+  static bool isCityOnly = false;
+
+  static String city = "Tokyo";
+  static String country = "Japan";
+
+  static String location = "";
+
+  static bool isLightMode = true;
+
+  static String accent = "";
+
+  static int unit = 3;
+
+
+}
