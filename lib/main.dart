8ad27809dbca6bee3dc2ee01a487099274b8f8a2
John Michael Goco <116796110+John-Michael-Goco@users.noreply.github.com>
2025-03-27 22:51:46 +0800

    Connect to Weather API

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,74 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(WeatherApp());
+  runApp(
+    CupertinoApp(
+        title: 'NavBook',
+        home: Homepage(),
+        debugShowCheckedModeBanner: false,
+        theme: CupertinoThemeData(
+            brightness: Brightness.light
+        )
+    ),
+  );
 }
 
-class WeatherApp extends StatelessWidget {
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
   @override
-  Widget build(BuildContext context) {
-    return CupertinoApp(
-      debugShowCheckedModeBanner: false,
-      home: WeatherScreen(),
-    );
+  State<Homepage> createState() => _HomepageState();
+}
+
+bool isCityOnly = true;
+
+class _HomepageState extends State<Homepage> {
+  @override
+
+  String city = "Tokyo";
+  String country = "Japan";
+  // String province = "PH-LUN"; // doesn't work, only for US province
+
+  // 1 = standard, 2 = imperial, 3 = metric
+  // OpenWeather api has &units 'query' to return specific units
+  Map<int, String> unitOptions = {
+    1: "standard",
+    2: "imperial",
+    3: "metric"
+  };
+
+  // Map for countries
+  Map<String, String> countryOptions = {
+    "Philippines": "PH",
+    "United States": "US",
+    "Japan": "JP",
+    "South Korea": "KR",
+    "United Kingdom": "UK"
+  };
+
+  Map<String, dynamic> weatherData = {};
+
+  Future<void> getWeatherData () async {
+
+    String? countryCode = countryOptions[country];
+    String baseUnit = unitOptions[3] ?? "standard";
+    String link = "https://api.openweathermap.org/data/2.5/weather?q="+city+",&units="+baseUnit+"&appid=ad66cda9e6fae9cf07de09f1301c1f37";
+    final weatherResponse = await http.get(Uri.parse(link)    );
+
+    weatherData = jsonDecode(weatherResponse.body);
+
+    // print(unitOptions[1]);
+    print(weatherData['name']);
+
   }
-}
-
-class WeatherScreen extends StatelessWidget {
   @override
+  void initState() {
+    getWeatherData();
+    super.initState();
+  }
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       backgroundColor: CupertinoColors.black,
@@ -26,7 +79,8 @@ class WeatherScreen extends StatelessWidget {
         ),
         trailing: CupertinoButton(
           padding: EdgeInsets.zero,
-          child: Icon(CupertinoIcons.settings, color: CupertinoColors.systemPurple),
+          child: Icon(
+              CupertinoIcons.settings, color: CupertinoColors.systemPurple),
           onPressed: () {},
         ),
         backgroundColor: CupertinoColors.black,
@@ -41,13 +95,15 @@ class WeatherScreen extends StatelessWidget {
             ),
             SizedBox(height: 4),
             Text(
-              'Russia',
+              'Location',
               style: TextStyle(color: CupertinoColors.white, fontSize: 18),
             ),
             SizedBox(height: 20),
             Text(
-              '-20°',
-              style: TextStyle(color: CupertinoColors.white, fontSize: 64, fontWeight: FontWeight.bold),
+              'Temperature',
+              style: TextStyle(color: CupertinoColors.white,
+                  fontSize: 64,
+                  fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 10),
             Icon(
@@ -57,12 +113,12 @@ class WeatherScreen extends StatelessWidget {
             ),
             SizedBox(height: 10),
             Text(
-              'broken clouds',
+              'Weather (Main)',
               style: TextStyle(color: CupertinoColors.white, fontSize: 18),
             ),
             SizedBox(height: 10),
             Text(
-              'H: 92% W: 3.81 kph',
+              'Extra Info',
               style: TextStyle(color: CupertinoColors.white, fontSize: 16),
             ),
           ],
@@ -71,3 +127,4 @@ class WeatherScreen extends StatelessWidget {
     );
   }
 }
+
